661b71ce4f26300cb260da24289d129c3d529520
Abhay Sharma <abhay240102@gmail.com>
2023-09-18 18:33:43 -0400

    fixed color bug and linting changes.
    Should work as intended now.

--- color_detection.py
+++ color_detection.py
@@ -1,5 +1,4 @@
 import cv2
-import math
 import numpy as np
 import pandas as pd
 import argparse
@@ -23,28 +22,16 @@ index = ["color", "color_name", "hex", "R", "G", "B"]
 csv = pd.read_csv('colors.csv', names=index, header=None)
 
 
-# function to calculate Euclidean Distance between 2 RGB values
-def euclideanDistance(color1, color2):
-    r1, g1, b1 = color1
-    r2, g2, b2 = color2
-    distance = math.sqrt((r1 - r2) ** 2 + (g1 - b2) ** 2 + (b1 - b2) ** 2)
-    return distance
-
-
-# function to calculate minimum distance from all colors and get the most matching color
-def getMatchingColor(clicked_rgb):
-    minimum = 1000
+# function to calculate minimum Euclidean Distance from all colors and get the most matching color
+def getMatchingColor(red, green, blue):
+    minimum = float("inf")
     cname = "Color"
-    for i in range(len(csv)):
-        r2 = int(csv.loc[i, "R"])
-        g2 = int(csv.loc[i, "G"])
-        b2 = int(csv.loc[i, "B"])
-        file_rgb = (r2, g2, b2)
-
-        distance = euclideanDistance(clicked_rgb, file_rgb)
-        if distance <= minimum:
+    for i in range(0, len(csv)):
+        distance = np.sqrt((red - int(csv.loc[i, "R"])) ** 2 + (green - int(csv.loc[i, "G"])) ** 2 + (
+                    blue - int(csv.loc[i, "B"])) ** 2)
+        if distance < minimum:
             minimum = distance
-            cname = csv.loc[i, "color_name"]    # TODO: Update color name at every click
+            cname = csv.loc[i, "color_name"]
     return cname
 
 
@@ -55,10 +42,11 @@ def drawFunction(event, x, y, flags, param):
         clicked = True
         xpos = x
         ypos = y
-        r, g, b = img[y, x]
+        b, g, r = img[y, x]
         r = int(r)
         g = int(g)
         b = int(b)
+
 
 cv2.namedWindow('image')
 cv2.setMouseCallback('image', drawFunction)
@@ -67,17 +55,17 @@ while True:
     cv2.imshow("image", img)
     if clicked:
         # cv2.rectangle(image, startpoint, endpoint, color, thickness)-1 fills entire rectangle
-        cv2.rectangle(img, (20,20), (750,60), (r,g,b), -1)
+        cv2.rectangle(img, (20, 20), (750, 60), (b, g, r), -1)
 
         # creating text string to display(Color name and RGB values)
-        text = getMatchingColor(clicked_img) + ' R=' + str(r) + ' G=' + str(g) + ' B=' + str(b)
+        text = getMatchingColor(r, g, b) + ' R=' + str(r) + ' G=' + str(g) + ' B=' + str(b)
 
         # cv2.putText(img,text,start,font(0-7),fontScale,color,thickness,lineType)
-        cv2.putText(img, text, (50,50), 2, 0.8, (255,255,255), 2, cv2.LINE_AA)
+        cv2.putText(img, text, (50, 50), 2, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
 
-        # for very light colours, display text in black colour
-        if r + g + b >= 600:
-            cv2.putText(img, text, (50,50), 2, 0.8, (0,0,0), 2, cv2.LINE_AA)
+        # for very light colors, display text in black color
+        if r + g + b >= 500:
+            cv2.putText(img, text, (50, 50), 2, 0.8, (0, 0, 0), 2, cv2.LINE_AA)
 
         clicked = False
 
